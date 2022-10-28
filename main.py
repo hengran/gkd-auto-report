923650fa64dac3ee739157802a219f59ef64cc47
ch1neiiiii <rachelwong2171@gmail.com>
2022-10-28 13:47:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def submit(s: requests.Session):
         "dezwjzyy": "",
         # 第三针接种
         "dszymjzqk": "2",                    # third vaccination situation  6.未接种
-        "dszjzsj": "2021-11-06",             # default time
+        "dszjzsj": "2021-11-02",             # default time
         "dszwjzyy": "",            # reason of non-vaccination
 
         "gtshryjkzk": "1",                   # health situation
